f64bb5efe88991e42a04cc948538a76a0cd961be
denys_gl <oggyla@gmail.com>
2020-12-20 22:44:31 -0800

    dropdown intial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
-const items = [
+// const items = [
+//   {
+//     title: "What is React",
+//     content: "React is a front end javascript framework"
+//   },
+//   {
+//     title: "Why use React",
+//     content: "React is a favorite JS library among engineers"
+//   },
+//   {
+//     title: "How do you use React",
+//     content: "You use React by creating components"
+//   }
+// ]
+
+const options = [
   {
-    title: "What is React",
-    content: "React is a front end javascript framework"
+    label: "The Color Red",
+    value: "red"
   },
   {
-    title: "Why use React",
-    content: "React is a favorite JS library among engineers"
+    label: "The Color Green",
+    value: "green"
   },
   {
-    title: "How do you use React",
-    content: "You use React by creating components"
+    label: "A Shade of Blue",
+    value: "blue"
   }
-]
-
+  ]
 export default () => {
+  const [selected, setSelected] = useState(options[0])
   return (
     <>
     {/* <Accordion items={items} /> */}
-    <Search />
+    {/* <Search /> */}
+    <Dropdown selected={selected} onSelectedChange={setSelected} options={options} />
     </>
   )
 }
