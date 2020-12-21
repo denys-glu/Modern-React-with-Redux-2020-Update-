172f3905a3236cd65d63c8b6795d7b25ee2c1a48
denys_gl <oggyla@gmail.com>
2020-12-20 21:55:09 -0800

    genious approach on how to throttle requests

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,40 +1,72 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
     const search = async () => {
-      const { data } = await axios.get('https://en.wikipedia.org/w/api.php',
-      {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
-        }
+          srsearch: debouncedTerm,
+        },
       });
-      setResults(data.query.search)
-    };
 
+      setResults(data.query.search);
+    };
     search();
-    
-  }, [term]);
+  }, [debouncedTerm]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
       <div className="ui form">
         <div className="field">
-          <label>Enter Search term</label>
-          <input className="input" type="text" value={term} onChange={e => setTerm(e.target.value)}/>
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
         </div>
-        {JSON.stringify(results)}
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
